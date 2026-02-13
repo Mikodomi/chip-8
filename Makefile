40f9497ec515968514a5ff47b0e49cb730ef6f95
Mikodomi <mikodominski@gmail.com>
2026-02-13 19:04:13 +0100

    added some more opcodes and began work on assembler

--- Makefile
+++ Makefile
@@ -1,6 +1,10 @@
-all: main.c
+all: interpreter assembler
+
+interpreter: main.c
 	gcc main.c -lSDL3 -o chip8
+
+assembler: assembler.c script
+	gcc assembler.c -o assembler -g
 
 debug: main.c test.ch8
 	gcc main.c -lSDL3 -o chip8 -g
-	gdb --command=init.gdb --args chip8 test.ch8 
--- main.c
+++ main.c
@@ -2,32 +2,55 @@
 #include <string.h>
 #include <inttypes.h>
 #include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
 #include <errno.h>
 
 #define MEM_SIZE 4096
 #define REG_COUNT 16
+#define SCREEN_WIDTH 64
+#define SCREEN_HEIGHT 32
 
-// takes original 16 bits as input (in big endian order
+// takes original 16 bits as input (in big endian order)
 #define REG1(x) (x & 0x000F)
 #define REG2(x) ((x & 0xF000) >> 8)
 #define CONST_VALUE(x) ((x & 0xFF00) >> 8)
 #define MEM_VALUE(x) (((x & 0xFF00) >> 8) | ((x & 0x000F) << 8))
 
 typedef struct {
+    // CHIP-8 logic
     uint8_t     mem[MEM_SIZE];
     uint8_t     *stack_pointer;
     uint8_t     v[REG_COUNT]; // data registers
     uint16_t    address; // 12-bit address register
     uint16_t    pc; // program counter
+    uint8_t     delay_timer;
+    uint8_t     sound_timer;
+    uint8_t     *screen;
+
+    // SDL
+    SDL_Window*     sdl_window;
+    SDL_Renderer*   sdl_renderer;
+    SDL_Texture*    sdl_texture;
+    SDL_Surface*    sdl_surface;
 
     uint16_t size;
 } chip8;
 
-int chip8_load_rom(chip8* machine, FILE* rom_input) {
-    memset(machine->mem, 0, MEM_SIZE);
-    size_t bytes_read = fread(machine->mem, sizeof(char), MEM_SIZE, rom_input);
+int chip8_load_rom(chip8* machine, FILE* rom_input, FILE* fonts_input) {
+    if (fonts_input) {
+        fread(machine->mem, sizeof(char), 0x50, fonts_input); // 16 five byte sprites = 0x50
+    }
+    size_t bytes_read = fread(&machine->mem[0x200], sizeof(char), MEM_SIZE-0x200, rom_input);
     machine->size = bytes_read;
     return bytes_read;
+}
+
+int chip8_screen_pixel_xor(chip8* machine, uint8_t x, uint8_t y, uint8_t val) {
+    if (x >= SCREEN_WIDTH || y >= SCREEN_HEIGHT) return -1;
+    if (!machine->screen) return -1;
+    machine->screen[y*SCREEN_WIDTH+x] ^= val;
+    return val;
 }
 
 int chip8_decode_2reg(chip8* machine, uint16_t instruction) {
@@ -56,6 +79,7 @@ int chip8_decode_2reg(chip8* machine, uint16_t instruction) {
             val1 -= val2;
             break;
         case 6:
+            val1 = val2;
             machine->v[0x0F] = (val1 & 0x01);
             val1 >>= 1;
             break;
@@ -64,6 +88,7 @@ int chip8_decode_2reg(chip8* machine, uint16_t instruction) {
             val1 = val2 - val1;
             break;
         case 0x0E:
+            val1 = val2;
             machine->v[0x0F] = (val1 & 0x80);
             val1 <<= 1;
             break;
@@ -87,6 +112,50 @@ int chip8_decode_zeroes(chip8* machine, uint16_t instruction) {
         default:
             machine->pc = bottom_bits / 2;
             break; // what does "jump to native assembler subroutine" even mean?
+    }
+    return status;
+}
+
+int chip8_F_instructions(chip8* machine, uint16_t instruction) {
+    int status = 0;
+    size_t reg1 = REG1(instruction);
+    uint16_t value = CONST_VALUE(instruction);
+    uint8_t temp;
+    switch (value) {
+        case 0x07:
+            machine->v[reg1] = machine->delay_timer;
+            break;
+        case 0x0A: break;
+        case 0x15: 
+            machine->delay_timer = machine->v[reg1];
+            break;
+        case 0x18: 
+            machine->sound_timer = machine->v[reg1];
+            break;
+        case 0x1E: 
+            machine->address += machine->v[reg1];
+            break;
+        case 0x29: break;
+        case 0x33: 
+            temp = machine->v[reg1];
+            (machine->mem[machine->address+2]) = temp % 10;
+            temp /= 10;
+            (machine->mem[machine->address+1]) = temp % 10;
+            temp /= 10;
+            (machine->mem[machine->address]) = temp % 10;
+            break;
+        case 0x55: 
+            for (int i = 0; i<=reg1; i++) {
+                machine->mem[machine->address+2*i] = machine->v[i];
+            }
+            break;
+        case 0x65: 
+            for (int i = 0; i<=reg1; i++) {
+                machine->v[i] = machine->mem[machine->address+2*i];
+            }
+            break;
+        default:
+            status = -1;
     }
     return status;
 }
@@ -153,11 +222,21 @@ int chip8_decode_execute(chip8* machine, uint16_t instruction) {
             addr = MEM_VALUE(instruction);      
             machine->address = addr;
             break;
-        case 0xB: break;
-        case 0xC: break;
-        case 0xD: break;
+        case 0xB: 
+            addr = MEM_VALUE(instruction) + machine->v[0];
+            machine->pc = addr / 2;
+            break;
+        case 0xC: 
+            reg1 = REG1(instruction);
+            value = CONST_VALUE(instruction);
+            machine->v[reg1] = rand() & value;
+            break;
+        case 0xD: 
+            break;
         case 0xE: break;
-        case 0xF: break;
+        case 0xF: 
+            chip8_F_instructions(machine, instruction);
+            break;
 
         default:
             status = -1;
@@ -167,8 +246,6 @@ int chip8_decode_execute(chip8* machine, uint16_t instruction) {
 
 
 int chip8_fde_cycle(chip8* machine) { // fetch decode execute
-    machine->pc = 0;
-    machine->stack_pointer = &machine->mem[0xEA0];
     int status = 0;
     uint16_t instruction;
     for (;2*machine->pc < machine->size;) {
@@ -178,6 +255,62 @@ int chip8_fde_cycle(chip8* machine) { // fetch decode execute
         if (status != 0) break;
     }
     return status;
+}
+
+void chip8_decrease_timer(chip8* machine) {
+    if (machine->delay_timer == 0) return;
+    machine->delay_timer--;
+}
+
+int chip8_init(chip8* machine, char* title) {
+    srand(time(NULL)); 
+    memset(machine, 0, sizeof(chip8));
+
+    machine->pc = 0x200;
+    machine->stack_pointer = &machine->mem[0xEA0];
+    machine->delay_timer = 0;
+
+    machine->screen = calloc(SCREEN_WIDTH/8*SCREEN_HEIGHT, sizeof(uint8_t));
+
+    if (!machine->screen) return -1;
+    // work in progress
+    /* if (!SDL_CreateWindowAndRenderer(title, 
+                SCREEN_HEIGHT, SCREEN_WIDTH,
+                SDL_WINDOW_RESIZABLE,
+                &machine->sdl_window, &machine->sdl_renderer)) {
+        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "%s", SDL_GetError());
+        return -1;       
+    }
+    machine->sdl_surface = SDL_CreateSurface(SCREEN_WIDTH, SCREEN_HEIGHT, SDL_PIXELFORMAT_INDEX1MSB);
+    if (!machine->sdl_surface) {
+        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "createsurface: %s", SDL_GetError());
+        return -1;
+    }
+
+    machine->sdl_texture = SDL_CreateTextureFromSurface(machine->sdl_renderer, machine->sdl_surface);
+    if (!machine->sdl_texture) {
+        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "createtexture: %s", SDL_GetError());
+        return -1;
+    } */
+    return 0;
+}
+
+void chip8_destroy(chip8* machine) {
+    if (SDL_WasInit(SDL_INIT_VIDEO)) {
+        if (machine->sdl_surface) {
+            SDL_DestroySurface(machine->sdl_surface);
+        }
+        if (machine->sdl_texture) {
+            SDL_DestroyTexture(machine->sdl_texture);
+        }
+        if (machine->sdl_renderer)  {
+            SDL_DestroyRenderer(machine->sdl_renderer);
+        }
+        if (machine->sdl_window) {
+            SDL_DestroyWindow(machine->sdl_window);
+        }
+    }
+    if (machine->screen) free(machine->screen);
 }
 
 int main(int argc, char **argv) {
@@ -190,11 +323,30 @@ int main(int argc, char **argv) {
         perror("fopen");
         return -1;
     }
+    FILE* fonts = fopen("font.ch8", "rb");
+    if (fonts == NULL) {
+        perror("fopen");
+        fprintf(stderr, "font file not found - fonts not available");
+    }
 
+    if (!SDL_Init(SDL_INIT_VIDEO)) {
+        fclose(rom);
+        return -1;
+    }
     chip8 machine;
-    chip8_load_rom(&machine, rom);
+    if (chip8_init(&machine, argv[1]) < 0) {
+        fprintf(stderr, "error during initialization\n");
+        fclose(rom);
+        chip8_destroy(&machine);
+        return -1;
+    }
+    chip8_load_rom(&machine, rom, fonts);
+    fclose(rom);
+    fclose(fonts);
+
     chip8_fde_cycle(&machine);
-    
-    fclose(rom);  
+
+    chip8_destroy(&machine);
+    SDL_Quit();
     return 0;
 }
