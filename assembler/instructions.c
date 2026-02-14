e225251ec87b9d844c717371f47cd4428cdd7f2d
Mikodomi <mikodominski@gmail.com>
2026-02-14 22:39:39 +0100

    added 8XYZ opcodes

--- assembler/instructions.c
+++ assembler/instructions.c
@@ -2,30 +2,72 @@
 #include <stdlib.h>
 #include <string.h>
 #include <assert.h>
+#include <inttypes.h>
+
+#define SPECIAL_CHARS " ;,\n"
 
 int assemble(FILE* in, FILE* out) {
     char* lineptr;
     size_t len = 0;
     ssize_t nread;
-    char* word;
-    char* word2;
+    char* instr;
+    char *op1, *op2, *op3;
     while ((nread = getline(&lineptr, &len, in)) != -1) {
-        if (lineptr[0] == ';') continue;
-        word = strtok(lineptr, " ;\n");
-        // 1 word instructions
-        if (strcmp(word, "clear") == 0) {
+        if (lineptr[0] == ';' || lineptr[0] == '\n') continue;
+        instr = strtok(lineptr, SPECIAL_CHARS);
+        if (!instr) break;
+        // instructions with no operand
+        if (strcmp(instr, "clear") == 0) {
             fwrite("\x00\xE0", 1, 2, out);
             continue;
-        } else if (strcmp(word, "ret") == 0) {
+        } else if (strcmp(instr, "ret") == 0) {
             fwrite("\x00\xEE", 1, 2, out);
             continue;
         }
-        word2 = strtok(NULL, " ;\n");
-        if (strcmp(word, "jmp") == 0 ) {
-            jmp_call(out, word2, INSTR_FLAG_JMP);
-        } else if (strcmp(word, "call") == 0) {
-            jmp_call(out, word2, INSTR_FLAG_CALL);
+        // 1 operand instructions
+        op1 = strtok(NULL, SPECIAL_CHARS);
+        if (strcmp(instr, "jmp") == 0 ) {
+            mem_instr(out, op1, INSTR_FLAG_JMP);
+            continue;
+        } else if (strcmp(instr, "call") == 0) {
+            mem_instr(out, op1, INSTR_FLAG_CALL);
+            continue;
+        } else if (strcmp(instr, "jv0") == 0) {
+            mem_instr(out, op1, INSTR_FLAG_JV0);
+            continue;
         }
+        // 2 operand instructions
+        op2 = strtok(NULL, SPECIAL_CHARS);
+        // there has to be a better way to do this
+        if (strcmp(instr, "mov") == 0) {
+            mov(out, op1, op2);
+            continue;
+        } else if (strcmp(instr, "or") == 0) {
+            reg_reg_instr(out, op1, op2, INSTR_FLAG_REG_REG_OR);
+            continue;
+        } else if (strcmp(instr, "and") == 0) {
+            reg_reg_instr(out, op1, op2, INSTR_FLAG_REG_REG_AND);
+            continue;
+        } else if (strcmp(instr, "xor") == 0) {
+            reg_reg_instr(out, op1, op2, INSTR_FLAG_REG_REG_XOR);
+            continue;
+        } else if (strcmp(instr, "add") == 0) {
+            reg_reg_instr(out, op1, op2, INSTR_FLAG_REG_REG_ADD);
+            continue;
+        } else if (strcmp(instr, "sub") == 0) {
+            reg_reg_instr(out, op1, op2, INSTR_FLAG_REG_REG_SUB);
+            continue;
+        } else if (strcmp(instr, "msr") == 0) {
+            reg_reg_instr(out, op1, op2, INSTR_FLAG_REG_REG_MSR);
+            continue;
+        } else if (strcmp(instr, "subs") == 0) {
+            reg_reg_instr(out, op1, op2, INSTR_FLAG_REG_REG_SUBS);
+            continue;
+        } else if (strcmp(instr, "msl") == 0) {
+            reg_reg_instr(out, op1, op2, INSTR_FLAG_REG_REG_MSL);
+            continue;
+        }
+        
     }
     free(lineptr);
     return 0;
@@ -72,16 +114,42 @@ int parse_number(char* word) {
     return parsed;
 }
 
-int jmp_call(FILE* out, char* address, instr_flags flag) {
-    assert(flag == INSTR_FLAG_JMP || flag == INSTR_FLAG_CALL);
+size_t write_BE(FILE* out, uint16_t in) {
+    char temp[2];
+    temp[0] = (in & 0xFF00) >> 8;
+    temp[1] = (in & 0x00FF);
+    return fwrite(temp, 1, 2, out);
+}
+
+int mem_instr(FILE* out, char* address, instr_flags flag) {
+    assert(flag == INSTR_FLAG_JMP || 
+            flag == INSTR_FLAG_CALL ||
+            flag == INSTR_FLAG_JV0 ||
+            flag == INSTR_FLAG_MOV_I);
     int number = parse_number(address);
     if (number >= 0x1000 || number < 0) return -1;
-    char res[2];
     number |= flag;
-    res[0] = (number & 0xFF00) >> 8;
-    res[1] = (number & 0x00FF);
-    size_t wrote = fwrite(res, 1, 2, out);
-    if (wrote != 2) return -1;
+    if (write_BE(out, number) != 2) return -1;
     return 0;
 }
 
+int reg_reg_instr(FILE* out, char* op1, char* op2, instr_flags flag) {
+    uint16_t reg1 = parse_digit(op1[1]);
+    if (reg1 < 0 || reg1 >= 0x10) return -1;
+    uint16_t reg2 = parse_digit(op2[1]);
+    if (reg2 < 0 || reg2 >= 0x10) return -1;
+    reg1 = ((reg1 << 8) | (reg2 << 4) | flag);
+    if (write_BE(out, reg1) != 2) return -1;
+    return 0;
+}
+
+int mov(FILE* out, char* op1, char* op2) {
+    if (!op1) return -1;
+    if (op1[0] == 'I') {
+        return mem_instr(out, op1, INSTR_FLAG_MOV_I);
+    }
+    if (op1[0] == 'v' && op2[0] == 'v') {
+        return reg_reg_instr(out, op1, op2, INSTR_FLAG_REG_REG_MOV); 
+    }
+    return -1; // 'mov dtm, ...' etc. unimplemented for now
+}
--- assembler/instructions.h
+++ assembler/instructions.h
@@ -5,6 +5,17 @@
 enum instr_flags_enum {
     INSTR_FLAG_JMP = 0x1000,
     INSTR_FLAG_CALL = 0x2000,
+    INSTR_FLAG_MOV_I = 0xA000,
+    INSTR_FLAG_JV0 = 0xB000,
+    INSTR_FLAG_REG_REG_MOV = 0x8000,
+    INSTR_FLAG_REG_REG_OR = 0x8001,
+    INSTR_FLAG_REG_REG_AND = 0x8002,
+    INSTR_FLAG_REG_REG_XOR = 0x8003,
+    INSTR_FLAG_REG_REG_ADD = 0x8004,
+    INSTR_FLAG_REG_REG_SUB = 0x8005,
+    INSTR_FLAG_REG_REG_MSR = 0x8006,
+    INSTR_FLAG_REG_REG_SUBS = 0x8007,
+    INSTR_FLAG_REG_REG_MSL = 0x800E,
 };
 
 typedef enum instr_flags_enum instr_flags;
@@ -13,6 +24,9 @@ int assemble(FILE* in, FILE* out);
 
 int parse_number(char* word);
 
-int jmp_call(FILE* out, char* line, instr_flags instr);
+int reg_reg_instr(FILE* out, char* op1, char* op2, instr_flags flag);
+int mem_instr(FILE* out, char* line, instr_flags instr);
+
+int mov(FILE* out, char* op1, char* op2);
 
 #endif
